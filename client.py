fae999da013e48f46dcaade2130305d5733ab730
Subham Kumar Dash <39568029+Subhamkumar23@users.noreply.github.com>
2020-04-08 20:07:22 +0530

    Socket Programming

---
+++ client.py
@@ -0,0 +1,28 @@
+import socket
+
+HEADER = 64
+PORT = 8000
+FORMAT = 'utf-8'
+SERVER = socket.gethostbyname(socket.gethostname())
+DISCONNECT_MESSAGE = "!DISCONNECT"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+send("Hello World!!")
+input()
+send("Hello World!!")
+input()
+send("Hello World!!")
+
+send(DISCONNECT_MESSAGE)
+
